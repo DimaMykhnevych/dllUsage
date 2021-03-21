f3fe082d5a88b34e9a6c81783c9f4beaab7d8daf
“Vlad-Fliahin“ <vvvf29012002@gmail.com>
2021-03-21 22:52:10 +0200

    Doesn't work(

--- Lab2/RSADLL/RSADLL.cpp
+++ Lab2/RSADLL/RSADLL.cpp
@@ -3,16 +3,21 @@
 #include <random>
 #include <string>
 #include <bitset>
+#include <iostream>
+
+typedef unsigned long long ull;
+typedef long double ld;
+
 using namespace std;
 
-unsigned long long getPrime() {
+ull getPrime() {
 	std::random_device rd;
 	std::default_random_engine generator(rd());
-	std::uniform_int_distribution<unsigned long long> distribution(3, 0xFFFFFFFF);
+	std::uniform_int_distribution<ull> distribution(3, 0xFFFFFFFF);
 
-	unsigned long long number = distribution(generator);
+	ull number = distribution(generator);
 	if (number % 2 == 0) number++;
-	for (unsigned long long i = 3; i * i <= number; i += 2)
+	for (ull i = 3; i * i <= number; i += 2)
 		if (number % i == 0) {
 			number += 2;
 			i = 1;
@@ -20,16 +25,16 @@ unsigned long long getPrime() {
 	return number;
 }
 
-unsigned long long gcd(unsigned long long x, unsigned long long y) {
+ull gcd(ull x, ull y) {
 	while (y != 0) {
-		unsigned long long r = x % y;
+		ull r = x % y;
 		x = y;
 		y = r;
 	}
 	return x;
 }
 
-string convertToBinary(unsigned long long num) {
+string convertToBinary(ull num) {
 	string r;
 	while (num != 0) 
 	{ 
@@ -39,107 +44,71 @@ string convertToBinary(unsigned long long num) {
 	return r;
 }
 
-
-template <class T>
-T mul_mod(T a, T b, T m) {
-	if (m == 0) return a * b;
-
-	T r = T();
-
-	while (a > 0) {
-		if (a & 1)
-			if ((r += b) > m) r %= m;
-		a >>= 1;
-		if ((b <<= 1) > m) b %= m;
-	}
-	return r;
+ull mul_mod(ull a, ull b, ull m) {
+	ld kd = ((ld)a * b) / m;
+	ull k = (ull)kd;
+	ull res = a * b - k * m;
+	return (res % m + m) % m;
 }
 
-template <class T>
-T pow_mod(T a, T n, T m) {
-	T r = 1;
+ull binaryPow(ull data, ull n, ull mod) {
+	ull r = 1;
 
-	while (n > 0) {
+	while (n) {
 		if (n & 1)
-			r = mul_mod(r, a, m);
-		a = mul_mod(a, a, m);
+			r = mul_mod(r, data, mod);
+		data = mul_mod(data, data, mod);
 		n >>= 1;
 	}
 	return r;
 }
 
-
-unsigned long long binaryPow(unsigned long long data, unsigned long long key, unsigned long long n) {
-
-
-	return pow_mod(data, key, n);
-
-	/*data %= n;
-	unsigned long long result = 1;
-	while (key > 0) {
-		if (key & 1) result = (unsigned long long)(result * data) % n;
-		data = (unsigned long long)(data * data) % n;
-		key >>= 1;
-	}
-	return result;*/
-
-	//string binary = convertToBinary(key);
-	//unsigned long long res = data;
-	//for (int i = 1; i < binary.size(); i++) {
-	//	if (binary[i] == '0') {
-	//		res = (res * res) % n;
-	//	}
-	//	else if (binary[i] == '1') {
-	//		res = (res * res) * data % n;
-	//	}
-	//}
-	//return res;
-
+ull phi(ull n) {
+	ull result = n;
+	for (ull i = 2; i <= (ull)sqrt(n); ++i)
+		if (n % i == 0) {
+			while (n % i == 0)
+				n /= i;
+			result -= result / i;
+		}
+	if (n > 1)
+		result -= result / n;
+	return result;
 }
 
-unsigned long long __stdcall generateKeys(unsigned long long& e, unsigned long long& d) {
-
-	unsigned long long p = getPrime();//  getPrime();//2999000009;
-	unsigned long long q = getPrime();// getPrime(); //2999000399 
+ull __stdcall generateKeys(ull& e, ull& d) {
+	ull p = getPrime();
+	ull q = getPrime();
 	while (p == q)
 	{
 		p = getPrime();
 	}
-	unsigned long long n = p * q;
-	unsigned long long fi = (p - 1) * (q - 1);
+	ull n = p * q;
+	ull fi = (p - 1) * (q - 1);
 
 	e = 3;
 	while (gcd(e, fi) != 1) {
 		e += 2;
 	}
 
+	ull  k = 1;
 
+	ull fi_phi = phi(fi);
+	d = binaryPow(e, fi_phi - 1, fi);
 
-	unsigned long long  k = 1;
-
-	while (true)
-	{
-		k = k + fi;
-
-		if (k % e == 0)
-		{
-			d = (k / e);
-			break;
-		}
-	}
-
+	cout << "p: " << p << " q: " << q << " fi:" << fi << " r: " << mul_mod(e, d, fi) << "\n";
 	return n;
 }
 
-bool __stdcall crypt(unsigned long long openMsg, unsigned long long e, 
-	unsigned long long n, unsigned long long* cryptMsg) {
+bool __stdcall crypt(ull openMsg, ull e, 
+	ull n, ull* cryptMsg) {
 	if (openMsg > n) return false;
 	*cryptMsg = binaryPow(openMsg, e, n);
 	return true;
 }
 
-bool __stdcall decrypt(unsigned long long cryptMsg, unsigned long long d,
-	unsigned long long n, unsigned long long* openMsg) {
+bool __stdcall decrypt(ull cryptMsg, ull d,
+	ull n, ull* openMsg) {
 	if (cryptMsg > n) return false;
 	*openMsg = binaryPow(cryptMsg, d, n);
 	return true;
